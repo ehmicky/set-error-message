76c603d5d7976f733db4979e564df802e0ee744b
ehmicky <ehmicky@gmail.com>
2022-08-28 23:33:41 +0200

    Add types documentation

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,9 +1,24 @@
 type NormalizeError<ErrorArg> = ErrorArg extends Error ? ErrorArg : Error
 
 /**
+ * Sets `error.message = newMessage`.
+ *
+ * Returns `error`. If `error` is not an `Error` in
+ * stance, it is converted to one.
+ *
+ * If `error.stack` contains `currentMessage`, it is replaced by `newMessage`.
+ * `currentMessage` is the error message currently included in `error.stack`. It
+ * defaults to `error.message`, which is usually best.
  *
  * @example
  * ```js
+ * const error = new Error('one')
+ * console.log(error.message) // 'one'
+ * console.log(error.stack) // 'Error: one ...'
+ *
+ * setErrorMessage(error, 'two')
+ * console.log(error.message) // 'two'
+ * console.log(error.stack) // 'Error: two ...'
  * ```
  */
 export default function setErrorMessage<ErrorArg>(
