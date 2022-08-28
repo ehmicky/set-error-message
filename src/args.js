75da2c790f1d91f401ae8f2de23dd76d181552e4
ehmicky <ehmicky@gmail.com>
2022-08-28 23:19:39 +0200

    Normalize arguments

---
+++ src/args.js
@@ -0,0 +1,16 @@
+// Normalize arguments
+export const normalizeArgs = function (
+  error,
+  newMessage,
+  currentMessage = error.message,
+) {
+  if (typeof newMessage !== 'string') {
+    throw new TypeError(`newMessage must be a string: ${newMessage}`)
+  }
+
+  if (typeof currentMessage !== 'string') {
+    throw new TypeError(`currentMessage must be a string: ${currentMessage}`)
+  }
+
+  return currentMessage
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import normalizeException from 'normalize-exception'
 
-export default function setErrorMessage(error) {
+import { normalizeArgs } from './args.js'
+
+export default function setErrorMessage(error, newMessage, currentMessage) {
   const errorA = normalizeException(error)
+  const currentMessageA = normalizeArgs(errorA, newMessage, currentMessage)
   return errorA
 }
