980f4f213e91e7f625122d036c5cf6de9a97826c
ehmicky <ehmicky@gmail.com>
2022-08-28 23:30:03 +0200

    Add more tests

---
+++ test/args.js
@@ -0,0 +1,32 @@
+import test from 'ava'
+import setErrorMessage from 'set-error-message'
+import { each } from 'test-each'
+
+// eslint-disable-next-line unicorn/no-null
+const NOT_A_MESSAGE = [null, true]
+
+each([...NOT_A_MESSAGE, undefined], ({ title }, notAMessage) => {
+  test(`Validate second argument | ${title}`, (t) => {
+    t.throws(setErrorMessage.bind(undefined, new Error('one'), notAMessage))
+  })
+})
+
+each(NOT_A_MESSAGE, ({ title }, notAMessage) => {
+  test(`Validate third argument | ${title}`, (t) => {
+    t.throws(setErrorMessage.bind(undefined, new Error('one'), '', notAMessage))
+  })
+})
+
+test('Third argument can be specified', (t) => {
+  const error = new Error('one two')
+  setErrorMessage(error, 'three', 'two')
+  t.true(error.stack.includes('one three'))
+})
+
+test('Third argument has a default value', (t) => {
+  const error = new Error('one two')
+  t.true(error.stack.includes('one two'))
+  error.message = 'two'
+  setErrorMessage(error, 'three')
+  t.true(error.stack.includes('one three'))
+})
