234c1975c5a527b42b6883bff077a8ce26d4fb2a
ehmicky <ehmicky@gmail.com>
2022-08-28 23:19:57 +0200

    Set error.message

--- src/main.js
+++ src/main.js
@@ -5,5 +5,16 @@ import { normalizeArgs } from './args.js'
 export default function setErrorMessage(error, newMessage, currentMessage) {
   const errorA = normalizeException(error)
   const currentMessageA = normalizeArgs(errorA, newMessage, currentMessage)
+  setNonEnumProp(error, 'message', newMessage)
   return errorA
+}
+
+const setNonEnumProp = function (error, propName, value) {
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(error, propName, {
+    value,
+    enumerable: true,
+    writable: true,
+    configurable: true,
+  })
 }
