2f2ad695d6dfe8c684f1f12fe6dd8ffeb27e38b7
ehmicky <ehmicky@gmail.com>
2022-08-28 23:19:06 +0200

    Use normalize-exception

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
-export default function setErrorMessage(value) {
-  return value
+import normalizeException from 'normalize-exception'
+
+export default function setErrorMessage(error) {
+  const errorA = normalizeException(error)
+  return errorA
 }
