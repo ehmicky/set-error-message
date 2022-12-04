d434a6ebfacc36f87d2421454c5b36f67a1a1f48
ehmicky <ehmicky@gmail.com>
2022-12-04 20:12:33 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,4 +1,4 @@
-import { expectType, expectAssignable, expectError } from 'tsd'
+import { expectType, expectAssignable } from 'tsd'
 
 import setErrorMessage from 'set-error-message'
 
@@ -6,9 +6,12 @@ const error = new Error('test')
 expectAssignable<Error>(setErrorMessage(error, ''))
 setErrorMessage(error, '')
 
-expectError(setErrorMessage(error))
-expectError(setErrorMessage(error, true))
-expectError(setErrorMessage(error, '', true))
+// @ts-expect-error
+setErrorMessage(error)
+// @ts-expect-error
+setErrorMessage(error, true)
+// @ts-expect-error
+setErrorMessage(error, '', true)
 
 expectAssignable<Error>(setErrorMessage(null, ''))
 expectType<true>(setErrorMessage(error as Error & { prop: true }, '').prop)
